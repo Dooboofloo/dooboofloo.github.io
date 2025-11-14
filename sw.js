93d878293dc3fda092afafa3a2c556ff7b44bd4b
Dooboofloo <38021265+Dooboofloo@users.noreply.github.com>
2025-11-13 21:41:08 -0600

    initial commit

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'mana-counter-cache-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './manifest.webmanifest',
+  './index.html',
+  './beleren-bold_P1.01.ttf',
+  './MTG_W.svg',
+  './MTG_U.svg',
+  './MTG_B.svg',
+  './MTG_R.svg',
+  './MTG_G.svg',
+  './MTG_C.svg',
+  './icon.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
